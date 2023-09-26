01f4646b7df3858c9d21a7aa12a733a0a9eec743
anezcjp <anezcjp@gmail.com>
2023-09-26 15:32:51 -0300

    Formatted Better

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const mysql = require("mysql");
 
-require('dotenv').config();
+require("dotenv").config();
 const app = express();
 
 app.use(cors());
@@ -26,81 +26,84 @@ db.connect((err) => {
   console.log("Connected to MySQL");
 });
 
-
 const users = [];
 
-
-app.post('/register', (req, res) => {
+app.post("/register", (req, res) => {
   const { username, email, password } = req.body;
 
-
-  db.query('SELECT * FROM users WHERE email = ?', [email], (err, results) => {
+  db.query("SELECT * FROM users WHERE email = ?", [email], (err, results) => {
     if (err) {
       console.error("Error checking for existing user: " + err);
-      return res.status(500).json({ error: 'Internal server error' });
+      return res.status(500).json({ error: "Internal server error" });
     }
 
     if (results.length > 0) {
-      return res.status(400).json({ error: 'User already exists' });
+      return res.status(400).json({ error: "User already exists" });
     }
 
-    db.query('INSERT INTO users (username, email, password) VALUES (?, ?, ?)', [username, email, password], (err, results) => {
-      if (err) {
-        console.error("Error inserting user into database: " + err);
-        return res.status(500).json({ error: 'Internal server error' });
-      }
+    db.query(
+      "INSERT INTO users (username, email, password) VALUES (?, ?, ?)",
+      [username, email, password],
+      (err, results) => {
+        if (err) {
+          console.error("Error inserting user into database: " + err);
+          return res.status(500).json({ error: "Internal server error" });
+        }
 
-      console.log(`New user registered: ${username}, ${email}`);
-      res.status(200).json({ message: 'Registration successful' });
-    });
+        console.log(`New user registered: ${username}, ${email}`);
+        res.status(200).json({ message: "Registration successful" });
+      }
+    );
   });
 });
 
-app.post('/login', (req, res) => {
+app.post("/login", (req, res) => {
   const { username, password } = req.body;
 
+  db.query(
+    "SELECT * FROM users WHERE username = ? AND password = ?",
+    [username, password],
+    (err, results) => {
+      if (err) {
+        console.error("Error checking user credentials: " + err);
+        return res.status(500).json({ error: "Internal server error" });
+      }
 
-  db.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], (err, results) => {
-    if (err) {
-      console.error("Error checking user credentials: " + err);
-      return res.status(500).json({ error: 'Internal server error' });
+      if (results.length === 1) {
+        console.log(`User logged in: ${username}`);
+        return res.status(200).json({ message: "Login successful" });
+      } else {
+        console.log(`Login failed for user: ${username}`);
+        return res.status(401).json({ error: "Unauthorized" });
+      }
     }
-
-    if (results.length === 1) {
-      console.log(`User logged in: ${username}`);
-      return res.status(200).json({ message: 'Login successful' });
-    } else {
-
-      console.log(`Login failed for user: ${username}`);
-      return res.status(401).json({ error: 'Unauthorized' });
-    }
-  });
+  );
 });
 
-app.post('/recovery', (req, res) => {
+app.post("/recovery", (req, res) => {
   const { username } = req.body;
 
+  db.query(
+    "SELECT email FROM users WHERE username = ?",
+    [username],
+    (err, results) => {
+      if (err) {
+        console.error("Error checking username: " + err);
+        return res.status(500).json({ error: "Internal server error" });
+      }
 
-  db.query('SELECT email FROM users WHERE username = ?', [username], (err, results) => {
-    if (err) {
-      console.error("Error checking username: " + err);
-      return res.status(500).json({ error: 'Internal server error' });
+      if (results.length === 1) {
+        const email = results[0].email;
+        console.log(`Email retrieved for username ${username}: ${email}`);
+        return res.status(200).json({ email });
+      } else {
+        console.log(`Username not found: ${username}`);
+        return res.status(404).json({ error: "Username not found" });
+      }
     }
-
-    if (results.length === 1) {
-
-      const email = results[0].email;
-      console.log(`Email retrieved for username ${username}: ${email}`);
-      return res.status(200).json({ email });
-    } else {
-
-      console.log(`Username not found: ${username}`);
-      return res.status(404).json({ error: 'Username not found' });
-    }
-  });
+  );
 });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
